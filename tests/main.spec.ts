05b757e1ae10ea52440f8a6f1f6c5b0ebb04b2f6
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-11 20:40:59 +0000

    chore: create initial test orchestrator

--- tests/main.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "@jest/globals";
-
-describe("testing jest integration", () => {
-  it("should be 2", () => {
-    const sum = 1 + 1
-    expect(sum).toBe(2)
-  })
-})
