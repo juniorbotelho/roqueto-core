a437445b3cc0277917727139d1f5bfb4efcc4829
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-12 13:18:43 +0000

    chore: add husky hooks configuration

---
+++ tests/main.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("main", () => {
+  it("should math result be the sum between two values", () => {
+    const sum = 1 + 1;
+    expect(sum).toBe(2);
+  });
+});
