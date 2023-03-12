6f57e2198799f310362e7e1e8e0e213e62351902
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-12 22:23:31 +0000

    chore: configure external lib

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,22 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.base.json");
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
   bail: false,
   detectLeaks: true,
   detectOpenHandles: true,
   maxConcurrency: 32,
   maxWorkers: "100%",
-
-  // Directories
   cacheDirectory: "<rootDir>/.cache",
   coverageDirectory: "<rootDir>/coverage",
-
   testMatch: [
     "<rootDir>/tests/**/?(*.)+(spec|test).[tj]s?(x)",
     "<rootDir>/src/?(*.)+(spec|test).[tj]s?(x)",
   ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
