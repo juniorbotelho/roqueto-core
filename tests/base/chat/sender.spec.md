7b68f7df9925653a038238b1303cad943a7153ba
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-12 20:57:34 +0000

    test: input invalid parameters must throw a error

--- tests/base/chat/sender.spec.md
+++ tests/base/chat/sender.spec.md
@@ -1,5 +1,8 @@
 Sender is the module thats wrapper logic to sending messages with whatsapp chat abstraction, the tests should be wrapper these todo list
 
 - must validate required input parameters
+  - input parameter should be a Sender object
+  - empty object parameter should throw a error
+
 - should return a schema with expected properties
 - should throw an exception based on known errors
--- tests/base/chat/sender.spec.ts
+++ tests/base/chat/sender.spec.ts
@@ -1,27 +1,59 @@
 import { describe, expect, it } from "@jest/globals";
 
+type SenderParam = {
+  username: string;
+  type: "chat" | "group";
+  message: string;
+  from: string;
+  to: string;
+};
+
 type Sender = {
   message: string;
 };
 
-const makeSut = () => {
-  const sender = async (): Promise<Sender> => {
+const sender = async (senderParam: SenderParam): Promise<Sender> => {
+  try {
+    if (senderParam.message.length <= 0) {
+      throw new Error("The message parameter doesn't be empty");
+    }
     return {
       message: "any_message",
     };
+  } catch (error) {
+    return error as Sender;
+  }
+};
+
+const makeSut = () => {
+  const senderMock = (senderMockParam?: Sender) => {
+    const senderParam: SenderParam = {
+      username: "any_username",
+      message: "any_message",
+      type: "chat",
+      from: "any_from_id",
+      to: "any_to_id",
+    };
+    return sender(Object.assign(senderParam, senderMockParam));
   };
 
   return {
-    sender,
+    senderMock,
   };
 };
 
 describe("chat message sending function", () => {
-  it("should return a schema with expected properties", () => {
+  it("input invalid parameters must throw a error", async () => {
     const sut = makeSut();
+    const res = await sut.senderMock({
+      message: "",
+    });
+    expect(res).toBeInstanceOf(Error);
+  });
 
-    const res = sut.sender();
-
-    expect(res).resolves.toHaveProperty("message", "any_message");
+  it("should return a schema with expected properties", async () => {
+    const sut = makeSut();
+    const res = await sut.senderMock();
+    expect(res).toHaveProperty("message", "any_message");
   });
 });
